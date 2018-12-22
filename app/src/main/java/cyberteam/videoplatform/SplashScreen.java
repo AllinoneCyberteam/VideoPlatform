4d1a8019c8f7c66271fd5e14415eb3763d911d38
Kumar Shivam Ray <kumarshivam060897@users.noreply.github.com>
2018-12-23 01:09:04 +0530

    Splash Screen activity ready to be initialized with the video, as of now the activity waits for 5 secs doing nothing and after 5 secs Login page appears.

--- app/src/main/java/cyberteam/videoplatform/SplashScreen.java
+++ app/src/main/java/cyberteam/videoplatform/SplashScreen.java
@@ -2,16 +2,29 @@ package cyberteam.videoplatform;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import cyberteam.videoplatform.login.Login;
 
 public class SplashScreen extends AppCompatActivity {
+    int splashTime = 5000;
+    Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        startActivity(new Intent(SplashScreen.this, Login.class));
+
+
+        // Initialize the splash screen here
+
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, Login.class));
+            }
+        }, splashTime);
     }
 }
