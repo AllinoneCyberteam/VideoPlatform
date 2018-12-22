13e02a5f20de9e1aece54aa7248d33d5c85b1d59
Kumar Shivam Ray <kumarshivam060897@users.noreply.github.com>
2018-12-22 21:06:02 +0530

    Initial commit

---
+++ app/src/main/java/cyberteam/videoplatform/CategorySelection.java
@@ -0,0 +1,13 @@
+package cyberteam.videoplatform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategorySelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_selection);
+    }
+}
