c045a5b01e2b18c51c76a4ae135b8d7ea0822a09
VaishnaviC25 <gaurichatap25@gmail.com>
2018-12-24 23:42:57 +0530

    video activity done

--- app/src/main/java/cyberteam/videoplatform/VideoActivity.java
+++ app/src/main/java/cyberteam/videoplatform/VideoActivity.java
@@ -2,12 +2,36 @@ package cyberteam.videoplatform;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class VideoActivity extends AppCompatActivity {
+import java.util.Vector;
+
+public class VideoActivity extends AppCompatActivity
+{
+
+    RecyclerView recyclerView;
+    Vector<YouTubeVideos> youtubeVideos = new Vector<YouTubeVideos>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView3);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager( new LinearLayoutManager(this));
+
+        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/ew5KolIPaPY\" frameborder=\"0\" allowfullscreen></iframe>") );
+        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.hotstar.com/embed/1000119060\" frameborder=\"0\" allowfullscreen></iframe>") );
+        youtubeVideos.add( new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/x1ed-0GjOZo\" frameborder=\"0\" allowfullscreen></iframe>") );
+
+        VideoAdapter videoAdapter = new VideoAdapter(youtubeVideos);
+        recyclerView.setAdapter(videoAdapter);
+
     }
+
+
 }
+
---
+++ app/src/main/java/cyberteam/videoplatform/YouTubeVideos.java
@@ -0,0 +1,17 @@
+package cyberteam.videoplatform;
+
+public class YouTubeVideos
+{
+    String videoUrl;
+    public YouTubeVideos() {
+    }
+    public YouTubeVideos(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+}
