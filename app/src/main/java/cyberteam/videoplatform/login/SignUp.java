0d50d365dd72f7068abfce8901209fd9633c8673
Kumar Shivam Ray <kumarshivam060897@users.noreply.github.com>
2018-12-30 17:07:13 +0530

    SignUp.java file is updated

--- app/src/main/java/cyberteam/videoplatform/login/SignUp.java
+++ app/src/main/java/cyberteam/videoplatform/login/SignUp.java
@@ -3,6 +3,7 @@ package cyberteam.videoplatform.login;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,6 +36,10 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
     Button SignUp;
     TextView goBack;
     TextView UserNameE;
+    TextView showPassword1;
+    TextView showPassword2;
+    boolean setType1 = true;
+    boolean setType2 = true;
     FirebaseAuth mAuth;
     FirebaseUser user;
     DatabaseReference mDatabaseReference;
@@ -62,6 +67,8 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
         SignUp.setOnClickListener(this);
         goBack.setOnClickListener(this);
+        showPassword1.setOnClickListener(this);
+        showPassword2.setOnClickListener(this);
     }
 
     @Override
@@ -69,47 +76,88 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         int id = view.getId();
         switch (id) {
             case R.id.SignUp: {
-                int arraySize = UserArrayList.size();
-                boolean avail = true;
-                for (int i = 0; i < arraySize; i++) {
-                    if (UserArrayList.get(i).equals(UserName.getText().toString())) {
-                        avail = false;
-                        break;
+                int arraySize;
+                boolean avail;
+                if (!UserName.getText().toString().equals("")) {
+                    arraySize = UserArrayList.size();
+                    avail = true;
+                    for (int i = 0; i < arraySize; i++) {
+                        if (UserArrayList.get(i).equals(UserName.getText().toString())) {
+                            avail = false;
+                            break;
+                        }
                     }
-                }
 
-                if (!avail) {
-                    UserNameE.setText(R.string.username_not_available);
-                    UserNameE.setTextColor(getResources().getColor(R.color.red));
-                    UserNameE.setVisibility(View.VISIBLE);
-                } else {
-                    UserNameE.setText(R.string.username_available);
-                    UserNameE.setTextColor(getResources().getColor(R.color.green));
-                    UserNameE.setVisibility(View.VISIBLE);
-
-                    if (!EmailId.getText().toString().equals("") && newPassword.getText().toString().equals(confirmPassword.getText().toString()) && !newPassword.getText().toString().equals("") && !confirmPassword.getText().toString().equals(""))
-                        mAuth.createUserWithEmailAndPassword(EmailId.getText().toString(), confirmPassword.getText().toString())
-                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<AuthResult> task) {
-                                        if (task.isSuccessful()) {
-                                            mAuth.signOut();
-                                            finish();
+                    if (!avail) {
+                        UserNameE.setText(R.string.username_not_available);
+                        UserNameE.setTextColor(getResources().getColor(R.color.red));
+                        UserNameE.setVisibility(View.VISIBLE);
+                    } else {
+                        UserNameE.setText(R.string.username_available);
+                        UserNameE.setTextColor(getResources().getColor(R.color.green));
+                        UserNameE.setVisibility(View.VISIBLE);
+                        if (!EmailId.getText().toString().equals("") && newPassword.getText().toString().equals(confirmPassword.getText().toString()) && !newPassword.getText().toString().equals("") && !confirmPassword.getText().toString().equals(""))
+                            mAuth.createUserWithEmailAndPassword(EmailId.getText().toString(), confirmPassword.getText().toString())
+                                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if (task.isSuccessful()) {
+                                                mAuth.signOut();
+                                                finish();
+                                            }
                                         }
-                                    }
-                                }).addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Toast.makeText(SignUp.this, "Something went wrong, try after some time", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    else
-                        Toast.makeText(SignUp.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
-                }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(SignUp.this, "Something went wrong, try after some time", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        else if (!newPassword.getText().toString().equals(confirmPassword.getText().toString()))
+                            Toast.makeText(SignUp.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                        else if (EmailId.getText().toString().equals(""))
+                            Toast.makeText(SignUp.this, "Enter your Email first", Toast.LENGTH_SHORT).show();
+                        else if (newPassword.getText().toString().equals(""))
+                            Toast.makeText(SignUp.this, "Password Field is empty", Toast.LENGTH_SHORT).show();
+                        else if (confirmPassword.getText().toString().equals(""))
+                            Toast.makeText(SignUp.this, "Confirm Password Field is empty", Toast.LENGTH_SHORT).show();
+                    }
+                } else Toast.makeText(SignUp.this, "Pick a Username", Toast.LENGTH_SHORT).show();
             }
             break;
             case R.id.Login: {
                 finish();
+                break;
+            }
+            case R.id.showPassword1: {
+                if (setType1) {
+                    setType1 = false;
+                    newPassword.setTransformationMethod(null);
+                    if (newPassword.getText().length() > 0)
+                        newPassword.setSelection(newPassword.getText().length());
+                    showPassword1.setBackgroundResource(R.drawable.hide_icon);
+                } else {
+                    setType1 = true;
+                    newPassword.setTransformationMethod(new PasswordTransformationMethod());
+                    if (newPassword.getText().length() > 0)
+                        newPassword.setSelection(newPassword.getText().length());
+                    showPassword1.setBackgroundResource(R.drawable.show_icon);
+                }
+                break;
+            }
+            case R.id.showPassword2: {
+                if (setType2) {
+                    setType2 = false;
+                    confirmPassword.setTransformationMethod(null);
+                    if (confirmPassword.getText().length() > 0)
+                        confirmPassword.setSelection(confirmPassword.getText().length());
+                    showPassword2.setBackgroundResource(R.drawable.hide_icon);
+                } else {
+                    setType2 = true;
+                    confirmPassword.setTransformationMethod(new PasswordTransformationMethod());
+                    if (confirmPassword.getText().length() > 0)
+                        confirmPassword.setSelection(confirmPassword.getText().length());
+                    showPassword2.setBackgroundResource(R.drawable.show_icon);
+                }
                 break;
             }
         }
@@ -124,6 +172,8 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
         goBack = findViewById(R.id.Login);
         mAuth = FirebaseAuth.getInstance();
         UserNameE = findViewById(R.id.userNameError);
+        showPassword1 = findViewById(R.id.showPassword1);
+        showPassword2 = findViewById(R.id.showPassword2);
         mDatabaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl(DatabaseLink);
     }
 
